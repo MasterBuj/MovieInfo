381e5692f297e4af7531b63d68be1306de4e9f0f
Lapi_Kun <Lapi_Kun@LAPIKUN>
2023-07-17 12:42:38 +0800

    v2 Update

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,7 +5,7 @@ const API_KEY = "&api_key=04c35731a5ee918f014970082a0088b1"
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc" + API_KEY
 
 const SEARCH_URL = BASE_URL + "/search/movie?" + API_KEY + "&query="
-
+NOW_PLAYING = BASE_URL + "/movie/now_playing?page=1" + API_KEY
 const FILTER_BY_GENRE = API_URL + "&with_genres="
 
 const GENRES = [{ "id": 28, "name": "Action" }, { "id": 12, "name": "Adventure" }, { "id": 16, "name": "Animation" }, { "id": 35, "name": "Comedy" }, { "id": 80, "name": "Crime" }, { "id": 99, "name": "Documentary" }, { "id": 18, "name": "Drama" }, { "id": 10751, "name": "Family" }, { "id": 14, "name": "Fantasy" }, { "id": 36, "name": "History" }, { "id": 27, "name": "Horror" }, { "id": 10402, "name": "Music" }, { "id": 9648, "name": "Mystery" }, { "id": 10749, "name": "Romance" }, { "id": 878, "name": "Science Fiction" }, { "id": 10770, "name": "TV Movie" }, { "id": 53, "name": "Thriller" }, { "id": 10752, "name": "War" }, { "id": 37, "name": "Western" }]
